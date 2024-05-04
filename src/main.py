822f7279910d53367d5ff2c1230ce0e38cecc177
BaDelhommer <badelhommer@gmail.com>
2024-05-03 23:34:08 -0500

    Finished the generate content step

--- src/main.py
+++ src/main.py
@@ -1,19 +1,29 @@
-from textnode import TextNode
-from htmlnode import HTMLNode
+import os
+import shutil
+
+from copystatic import copy_files_recursive
+from gencontent import generate_page
+
+dir_path_static = "./static"
+dir_path_public = "./public"
+dir_path_content = "./content"
+template_path = "./template.html"
+
 
 def main():
-    text_node1 = TextNode("jeff", "bold", "boot.dev")
-    text_node2 = TextNode("jeff", "bold", "boot.dev")
-    text_node3 = TextNode("John", "italics", "bootdev.com")
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
 
-    html_node = HTMLNode(props={"href": "https://www.google.com", "target": "_blank"})
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
 
-    print(html_node.props_to_html())
+    print("Generating page...")
+    generate_page(
+        os.path.join(dir_path_content, "index.md"),
+        template_path,
+        os.path.join(dir_path_public, "index.html"),
+    )
 
-    print(text_node1.__repr__)
-    print(text_node1.__eq__(text_node2))
-    print(text_node1.__eq__(text_node3))
 
-
-if __name__ == "__main__":
-    main()
+main()
